468166c1edbd5cd3f3dfa3b7daa6cff12d700aae
Rik <rdewijn@xs4all.nl>
2021-03-20 16:56:59 +0000

    first commit

--- server.R
+++ server.R
@@ -1,7 +1,12 @@
 library(shiny)
 library(tercen)
-library(dplyr)
-library(tidyr)
+library(tidyverse)
+library(globaltest)
+library(reshape2)
+library(data.table)
+library(ggsci)
+
+
 
 ############################################
 #### This part should not be modified
@@ -18,36 +23,170 @@ getCtx <- function(session) {
 ####
 ############################################
 
-shinyServer(function(input, output, session) {
+server <- shinyServer(function(input, output, session) {
   
-  dataInput <- reactive({
+  db = reactive({
+    upstreamDb()
+  })
+  
+  s1 = reactive({
+    TableS1()
+  })
+  
+  dataIn = reactive({
     getValues(session)
   })
   
-  output$reacOut <- renderUI({
-    plotOutput(
-      "main.plot",
-      height = input$plotHeight,
-      width = input$plotWidth
-    )
-  }) 
-  
-  output$main.plot <- renderPlot({
-    values <- dataInput()
-    data <- values$data$.y
-    hist(data)
+  observe({
+    dfin = dataIn()
+    
+    selectMappings = reactive({
+      tabs1 = s1() %>%
+        filter(GroupName == "TK")
+      db() %>%
+        ungroup() %>% 
+        filter(PepProtein_SeqHomology >= input$seqhom) %>%
+        filter(family == "PTK") %>% 
+        filter(Kinase_Name %in% tabs1$KR_Name) %>%
+        filter(ID %in% dfin$ID)
+    })
+    
+    scores = reactive({
+      dbi = selectMappings() %>% 
+        scoreIviv(input$iviv)
+      dbp = selectMappings() %>%
+        scorePNet(ranks = input$pn.breakpoints, scores = c(input$pn.low, input$pn.low, input$pn.high))
+      combinedScores(list(dbi, dbp), input$p0) %>%
+        normalizeScores()
+    })
+    
+    kinasetest = reactive({
+      X = acast(dfin, .ci ~ ID, value.var = ".y")
+      grp = acast(dfin, .ci ~ ID, value.var = "grp")[,1]
+      result = scores() %>%
+        filter(sc.nor > input$thr) %>%
+        group_by(Kinase_Name) %>%
+        do(gtest(.,X, grp))
+    })
+    
+    getResultTable = reactive({
+      kinasetest() %>%
+        filter(N >= input$pss) %>%
+        arrange(p) %>%
+        left_join(s1(), by = c(Kinase_Name = "KR_Name")) %>%
+        mutate(fdr = p.adjust(p, "fdr"))%>%
+        select(Kinase_Name, N, delta, p, fdr, Family) 
+    })
+    
+    output$kup = renderTable({
+        getResultTable() %>% 
+        filter(delta >= 0)
+    })
+    
+    output$kdn = renderTable({
+        getResultTable() %>%
+        filter(delta < 0)
+    })
+    
+    output$vplot = renderPlot({
+      result = getResultTable()  
+      p = ggplot(result, aes(x = delta, y = -log10(fdr), label = Kinase_Name, size = N, colour =Family ))
+        p = p + geom_text() + theme_bw()
+        p = p + xlab("delta") + ylab("-log10(fdr)") + guides(color  = FALSE)
+        #p = p + xlim( max(abs(result$delta)) *c(-1.1, 1,1) ) + ylim( c(0, max(-log10(result$fdr))) )
+        print(p)
+    })
+    
   })
   
 })
 
 getValues <- function(session){
   ctx <- getCtx(session)
-  values <- list()
+  df <- ctx %>% 
+    select(.y, .ri, .ci)
+  if(length(ctx$colors) == 1){
+    df = df %>% bind_cols(data.frame(grp = ctx$select(ctx$colors)[[1]]))
+  } else {
+    stop("Need axctly one color for the grouping")
+  }
+  if (!("ID" %in% ctx$rnames[[1]]) )stop("Factor ID for identifying spots is required")
+  id = data.frame(.ri =unique(df$.ri), ID=ctx%>%rselect("ID"))
   
-  values$data <- ctx %>% select(.y, .ri, .ci) %>%
-    group_by(.ci, .ri) %>%
-    summarise(.y = mean(.y)) # take the mean of multiple values per cell
+  df = df %>% 
+    left_join(id, by = ".ri") %>%
+    mutate( across(!where(is.numeric), as.factor)) %>%
+    arrange(.ri, .ci)
+}
+
+upstreamDb = function(){
+  load("./d/180509_86312_86402_87102_UpstreamDb.RData")
+  UpstreamDatabase
+}
+
+TableS1 = function(){
+  df = read.delim("./d/TableS1 Kinases in Kinome Render.txt")
+}
+
+scoreIviv = function(db, score){
+  db %>% 
+    filter(Database == "iviv") %>%
+    mutate(s = score)
+}
+
+scorePNet = function(db, ranks, scores){
+  db %>% 
+    mutate(
+      s  = case_when(
+        Kinase_Rank > ranks[2] ~ scores[3],
+        Kinase_Rank > ranks[1] ~ scores[2],
+        TRUE ~ scores[1])
+    )
+}
+
+combinedScores = function(dblist, minscore){
+  dbc = bind_rows(dblist) %>%
+    group_by(Kinase_Name, ID) %>%
+    summarise(sc = 1-prod(1-s))
   
-  return(values)
+  db.all = expand.grid(levels(droplevels(dbc$Kinase_Name)), levels(droplevels(dbc$ID)) )
+  colnames(db.all) = c("Kinase_Name", "ID")
+  
+  db.all %>%
+    left_join(dbc, by = c("Kinase_Name", "ID")) %>%
+    mutate(sc.final = case_when(
+      is.na(sc) ~ minscore,
+      sc < minscore ~ minscore,
+      TRUE ~ sc))
+}
+
+normalizeScores = function(db){
+  sumdf = db %>% group_by(ID) %>% summarise(sumsc = sum(sc.final))
+  db %>%
+    left_join(sumdf, by = "ID") %>%
+    group_by(ID) %>%
+    mutate(sc.nor = sc.final/sumsc)
+}
+
+gtest = function(kinase.set, X, grp){
+  bset = colnames(X) %in% kinase.set$ID
+  X = X[, bset,drop = FALSE]
+  if(dim(X)[2]>0){
+    globtest = gt(grp ~ X, directional = TRUE, standardize = TRUE)
+    p = attr(globtest, "result")[1,1]
+    delta = mean(gdelta(X, grp))
+  } else {
+    p = NaN
+    delta = NaN
+  }
+  result = data.table(p = p, N = dim(X)[2], delta = delta, gt = list(globtest))
+}
+
+gdelta = function(Xin, grp){
+  X1 = Xin[grp == levels(grp)[1],,drop = FALSE]
+  X2 = Xin[grp == levels(grp)[2],,drop= FALSE]
+  M1 =apply(X1, 2, mean)
+  M2 =apply(X2, 2, mean)
+  delta = M2 - M1
 }
 
--- ui.R
+++ ui.R
@@ -1,16 +1,79 @@
 library(shiny)
+library(shinydashboard)
 
-shinyUI(fluidPage(
-  
-  titlePanel("Histogram"),
-  
-  sidebarPanel(
-    sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
-    sliderInput("plotHeight", "Plot height (px)", 200, 2000, 500),
-  ),
-  
-  mainPanel(
-    uiOutput("reacOut")
+ui = shinyUI
+(
+  fluidPage
+  (
+    
+    ui <- dashboardPage
+    (
+      dashboardHeader(title = "supertest"),
+      dashboardSidebar(
+        sidebarMenu(
+          menuItem("home", tabName = "home", icon = icon("home")),
+          menuItem("results table", tabName = "results", icon = icon("th")),
+          menuItem("volcano plot", tabName = "vp", icon = icon("image")),
+          menuItem("settings", tabName = "settings", icon = icon("dashboard"))
+        )
+      )
+      ,
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "home",
+                  fluidRow(
+                    box(title = "Simple UPstream kinase EnRichment TEST")
+                  )
+          ),
+          
+          tabItem(tabName = "settings",
+                  # Boxes need to be put in a row (or column)
+                  
+                  fluidRow(
+                    box(title = "General Settings",
+                        sliderInput("seqhom", "Minimal Sequence Homolgy:",0,1, 0.9,),
+                        sliderInput("p0", "Minimal confidence:", 0,1,0.25),
+                        sliderInput("thr", "Specificity threshold:", 0, 0.08, 0.018),
+                        sliderInput("pss", "Minimum peptide set size:", 0, 6, 4)
+                    ),
+                    fluidRow(
+                      
+                      box(
+                        title = "In Vivo In Vitro DB settings",
+                        sliderInput("iviv", "Confidence:", 0,1, 0.9)
+                      ),
+                      box(
+                        title = "PhosphoNET DB settings",
+                        sliderInput("pn.breakpoints", "Rank breakpoints (define mid range): ", 1,25,value = c(4,12)),
+                        sliderInput("pn.low", "Low rank confidence", 0,1,0.9),
+                        sliderInput("pn.mid", "Mid rank confidence", 0,1,0.7),
+                        sliderInput("pn.high", "Low rank confidence", 0,1,0.25)
+                      )
+                    )
+                  )
+          ),
+          tabItem(tabName = "results",
+                  fluidRow(
+                    box(title = "Activity up",
+                        tableOutput("kup")),
+                    box(title = "Activity down",
+                        tableOutput("kdn"))
+                    
+                  )
+          ),
+          tabItem(tabName = "vp",
+                  fluidRow(
+                    box(width = 8,
+                        height = 12,
+                        plotOutput("vplot")
+                    )
+                  )
+          )
+          
+          
+        )
+        
+      )
+    )
   )
-  
-))
+)
