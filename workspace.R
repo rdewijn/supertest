50d19e87d2bba75ae88b0ece6ec1064bcae09f6a
rdw <rdwijn@pamgene.com>
2021-11-12 09:40:19 +0000

    ++

--- workspace.R
+++ workspace.R
@@ -1,14 +1,14 @@
-library(shiny)
-library(tercen)
-library(dplyr)
-library(tidyr)
-
-
-############################################
-
-source("ui.R")
-source("server.R")
-options("tercen.workflowId"= "3a85f3a46cd4009b31373508220064f2")
-options("tercen.stepId"= "86799058-7709-4ec3-adc5-41a4ac6e3c11")
-
-runApp(shinyApp(ui, server))  
+# library(shiny)
+# library(tercen)
+# library(dplyr)
+# library(tidyr)
+# 
+# 
+# ############################################
+# 
+# source("ui.R")
+# source("server.R")
+# options("tercen.workflowId"= "3a85f3a46cd4009b31373508220064f2")
+# options("tercen.stepId"= "86799058-7709-4ec3-adc5-41a4ac6e3c11")
+# 
+# runApp(shinyApp(ui, server))  
